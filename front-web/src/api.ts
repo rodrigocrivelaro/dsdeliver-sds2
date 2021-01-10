80852e456dcd96c4fe142d62d55b5e6b424aa010
Rodrigo Crivelaro <rreiscrivelaro@gmail.com>
2021-01-09 21:32:43 -0300

    Add env var

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { OrderPayLoad } from './Orders/types';
-const API_URL = "http://localhost:8080";
+const API_URL = process.env.REACT_APP_API_URL
 // const API_URL = "https://crivelaro-sds2.herokuapp.com";
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
